c2faed9deafb99e24c9be2ee9586bac5b0b8459c
Ante Jukica <ajukica@gmail.com>
2024-02-14 13:48:26 +0100

    Added footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type ColumnProps = {
+  title: String;
+  links: Array<string>;
+};
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
+  <div className="footer-column">
+    <h4 className="font-semibold">{title}</h4>
+    <ul className="flex flex-col gap-2 font-normal">
+      {links.map((link) => (
+        <Link href="/" key={link}>
+          {" "}
+          {link}{" "}
+        </Link>
+      ))}
+    </ul>
+  </div>
+);
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+            src="/logo-purple.svg"
+            alt="Devstack Logo"
+            width={115}
+            height={38}
+          />
 
-export default Footer
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">
+            Community for sharing developers work
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-12">
+          <FooterColumn
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
+          />
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
